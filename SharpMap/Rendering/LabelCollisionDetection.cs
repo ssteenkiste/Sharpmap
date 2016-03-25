7cff3bf91349e5e7577b8a00b2006af7510e7f02
Sébastien Steenkiste <ssteenkiste@eurisis.fr>
2016-03-25 14:40:34 +0100

    Label collision detection

--- SharpMap/Rendering/LabelCollisionDetection.cs
+++ SharpMap/Rendering/LabelCollisionDetection.cs
@@ -16,6 +16,9 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
 
 using System.Collections.Generic;
+using System.Linq;
+using System;
+using SharpMap.Utilities.Indexing;
 
 namespace SharpMap.Rendering
 {
@@ -51,9 +54,11 @@ namespace SharpMap.Rendering
                     if (labels[i].Priority == labels[i - 1].Priority) continue;
 
                     if (labels[i].Priority > labels[i - 1].Priority)
-                        labels.RemoveAt(i - 1);
+                        //labels.RemoveAt(i - 1);
+                        labels[i - 1].Show = false;
                     else
-                        labels.RemoveAt(i);
+                        //labels.RemoveAt(i)
+                        labels[i].Show = false;
                 }
         }
 
@@ -86,6 +91,127 @@ namespace SharpMap.Rendering
                             break;
                         }
                 }
+            }
+        }
+
+        /// <summary>
+        /// Quick (O(n log n)) and accurate collision detection
+        /// </summary>
+        /// <param name="labels"></param>
+        public static void QuickAccurateCollisionDetectionMethod(List<BaseLabel> labels)
+        {
+            // find minimum / maximum coordinates
+            double minX = double.MaxValue;
+            double maxX = double.MinValue;
+            double minY = double.MaxValue;
+            double maxY = double.MinValue;
+            foreach (BaseLabel l in labels)
+            {
+                ProperBox box = new ProperBox(l.Box);
+                if (box.Left < minX) minX = box.Left;
+                if (box.Right > maxX) maxX = box.Right;
+                if (box.Bottom > maxY) maxY = box.Bottom;
+                if (box.Top < minY) minY = box.Top;
+            }
+
+            // sort by area (highest priority first, followed by low area to maximize the amount of labels displayed)
+            var sortedLabels = labels.OrderByDescending(label => label.Priority).ThenBy(label => label.Box.Width * label.Box.Height);
+
+            // make visible if it does not collide with other labels. Uses a quadtree and is therefore fast O(n log n) 
+            QuadtreeNode<ProperBox> quadTree = new QuadtreeNode<ProperBox>(minX, maxX, minY, maxY, new LabelBoxContainmentChecker(), 0, 10);
+            foreach (BaseLabel l in sortedLabels)
+            {
+                if (!l.Show) continue;
+                if (quadTree.CollidesWithAny(new ProperBox(l.Box)))
+                {
+                    l.Show = false;
+                }
+                else
+                {
+                    ProperBox box = new ProperBox(l.Box);
+                    quadTree.Insert(box);
+                }
+            }
+        }
+
+        #endregion
+
+        #region "Tools"
+
+        private class ProperBox
+        {
+            private LabelBox box;
+
+            /// <summary>
+            /// Initialize a new instance of <see cref="ProperBox"/>.
+            /// </summary>
+            /// <param name="box"></param>
+            public ProperBox(LabelBox box)
+            {
+                this.box = box;
+            }
+
+            public float Left
+            {
+                get { return box.Left; }
+            }
+
+            public float Right
+            {
+                get { return box.Right; }
+            }
+
+            public float Height
+            {
+                get { return box.Height; }
+            }
+
+            public float Width
+            {
+                get { return box.Width; }
+            }
+
+            public float Bottom
+            {
+                get { return box.Top + Height; }
+            }
+
+            public float Top
+            {
+                get { return box.Top; }
+            }
+        }
+
+        /// <summary>
+        /// A labelbox checker.
+        /// </summary>
+        private class LabelBoxContainmentChecker : QuadtreeNode<ProperBox>.QuadtreeContainmentChecker<ProperBox>
+        {
+            /// <summary>
+            /// Check if node contains or intersects the box.
+            /// </summary>
+            /// <param name="node"></param>
+            /// <param name="obj"></param>
+            /// <returns></returns>
+            public bool IsContainedOrIntersects(QuadtreeNode<ProperBox> node, ProperBox obj)
+            {
+                return !(obj.Left > node.Right | obj.Right < node.Left | obj.Top > node.Bottom | obj.Bottom < node.Top);
+            }
+
+            /// <summary>
+            /// Check if
+            /// </summary>
+            /// <param name="node"></param>
+            /// <param name="obj"></param>
+            /// <returns></returns>
+            public bool IsFullyContained(QuadtreeNode<ProperBox> node, ProperBox obj)
+            {
+                return obj.Left >= node.Left & obj.Right <= node.Right & obj.Bottom <= node.Bottom & obj.Top >= node.Top;
+            }
+
+            public bool IsContainedOrIntersects(ProperBox obj1, ProperBox obj2)
+            {
+                return !(obj1.Left > obj2.Right | obj1.Right < obj2.Left | obj1.Top > obj2.Bottom | obj1.Bottom < obj2.Top);
             }
         }
 
---
+++ SharpMap/Utilities/Indexing/QuadTree.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpMap.Utilities.Indexing
+{
+    /// <summary>
+    /// A quad tree node.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class QuadtreeNode<T>
+    {
+        List<QuadtreeNode<T>> children = null;
+        List<T> objects;
+        int maxObjects;
+        int level;
+        int maxLevel;
+
+        /// <summary>
+        /// Gets or sets the left position.
+        /// </summary>
+        public double Left { get; set; }
+
+        /// <summary>
+        /// Gets or sets the right position.
+        /// </summary>
+        public double Right { get; set; }
+
+        /// <summary>
+        /// Gets or sets the right position.
+        /// </summary>
+        public double Top { get; set; }
+
+        /// <summary>
+        /// Gets or sets the bottom position.
+        /// </summary>
+        public double Bottom { get; set; }
+
+        protected QuadtreeContainmentChecker<T> containmentChecker;
+
+        public const int DEFAULT_MAX_OBJECTS_PER_NODE = 5;
+
+        /// <summary>
+        /// Initialize a new instance of <see cref="QuadtreeNode{T}"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <param name="top"></param>
+        /// <param name="bottom"></param>
+        /// <param name="containmentChecker"></param>
+        /// <param name="maxObjects"></param>
+        /// <param name="level"></param>
+        /// <param name="maxLevel"></param>
+        public QuadtreeNode(double left, double right, double top, double bottom, QuadtreeContainmentChecker<T> containmentChecker, int maxObjects, int level, int maxLevel)
+        {
+            this.Left = left;
+            this.Right = right;
+            this.Top = top;
+            this.Bottom = bottom;
+            this.containmentChecker = containmentChecker;
+            this.maxObjects = maxObjects;
+            this.objects = new List<T>(maxObjects);
+            this.level = level;
+            this.maxLevel = maxLevel;
+        }
+
+        /// <summary>
+        /// Initialize a new instance of <see cref="QuadtreeNode{T}"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <param name="top"></param>
+        /// <param name="bottom"></param>
+        /// <param name="containmentChecker"></param>
+        /// <param name="level"></param>
+        /// <param name="maxLevel"></param>
+        public QuadtreeNode(double left, double right, double top, double bottom, QuadtreeContainmentChecker<T> containmentChecker, int level, int maxLevel)
+            : this(left, right, top, bottom, containmentChecker, QuadtreeNode<T>.DEFAULT_MAX_OBJECTS_PER_NODE, level, maxLevel)
+        { }
+
+        /// <summary>
+        /// Insert an object.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool Insert(T obj)
+        {
+            if (this.IsFullyContained(obj))
+            {
+                if (this.IsLeaf())
+                {
+                    if (objects.Count >= maxObjects && level < maxLevel)
+                    {
+                        this.Divide();
+                        if (!this.InsertIntoChildren(obj)) this.objects.Add(obj);
+                    }
+                    else
+                    {
+                        this.objects.Add(obj);
+
+                    }
+                }
+                else
+                {
+                    if (!this.InsertIntoChildren(obj)) this.objects.Add(obj);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsFullyContained(T obj)
+        {
+            return this.containmentChecker.IsFullyContained(this, obj);
+        }
+
+        public bool CollidesWithOrContains(T obj)
+        {
+            return this.containmentChecker.IsContainedOrIntersects(this, obj);
+        }
+
+        public bool IsLeaf()
+        {
+            return this.children == null;
+        }
+
+        /// <summary>
+        /// Get the childrens.
+        /// </summary>
+        /// <returns></returns>
+        public List<QuadtreeNode<T>> GetChildren()
+        {
+            return this.children;
+        }
+
+        public bool CollidesWithAny(T obj)
+        {
+            if (this.CollidesWithOrContains(obj))
+            {
+                //collides with any of current level?
+                foreach (T o in this.objects)
+                {
+                    if (this.containmentChecker.IsContainedOrIntersects(o, obj)) return true;
+                }
+
+                //collides with a child?
+                if (!this.IsLeaf())
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (children[i].CollidesWithAny(obj)) return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Gets objects relative to position.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public List<T> GetObjects(double x, double y)
+        {
+            if (this.IsLeaf())
+            {
+                return this.objects;
+            }
+            else
+            {
+                foreach (QuadtreeNode<T> node in this.children)
+                {
+                    if (x >= node.Left && x <= node.Right && y >= node.Bottom && y <= node.Top)
+                    {
+                        return node.GetObjects(x, y);
+                    }
+                }
+            }
+            return new List<T>();
+        }
+
+        /// <summary>
+        /// Insert into children.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        protected bool InsertIntoChildren(T obj)
+        {
+            bool inserted = false;
+            for (int i = 0; i < 4; i++)
+            {
+                if (children[i].Insert(obj)) inserted = true;
+            }
+            return inserted;
+        }
+
+        /// <summary>
+        /// Divide node in quad nodes.
+        /// </summary>
+        protected void Divide()
+        {
+            this.children = new List<QuadtreeNode<T>>(4);
+            double middleX = Left + ((Right - Left) / 2);
+            double middleY = Top + (Math.Abs(Top - Bottom) / 2);
+            this.children.Add(new QuadtreeNode<T>(Left, middleX, Top, middleY, containmentChecker, maxObjects, level + 1, maxLevel));
+            this.children.Add(new QuadtreeNode<T>(middleX, Right, Top, middleY, containmentChecker, maxObjects, level + 1, maxLevel));
+            this.children.Add(new QuadtreeNode<T>(Left, middleX, middleY, Bottom, containmentChecker, maxObjects, level + 1, maxLevel));
+            this.children.Add(new QuadtreeNode<T>(middleX, Right, middleY, Bottom, containmentChecker, maxObjects, level + 1, maxLevel));
+
+            //move objects to child nodes
+            List<T> remainingObjects = new List<T>();
+            foreach (T obj in this.objects)
+            {
+                if (!this.InsertIntoChildren(obj)) remainingObjects.Add(obj);
+            }
+            this.objects = remainingObjects;
+        }
+
+        /// <summary>
+        /// Interface for quad tree containment checker.
+        /// </summary>
+        /// <typeparam name="P"></typeparam>
+        public interface QuadtreeContainmentChecker<P>
+        {
+            bool IsContainedOrIntersects(QuadtreeNode<P> node, P obj);
+
+            bool IsContainedOrIntersects(P obj1, P obj2);
+
+            bool IsFullyContained(QuadtreeNode<P> node, P obj);
+        }
+    }
+
+}
